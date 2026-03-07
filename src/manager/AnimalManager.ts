9c8a915078563ac88dfc662e06c26d8fbf45ea47
tazoakhalaia <darknemiroff@gmail.com>
2026-03-07 20:46:05 +0400

    remove unused files

--- src/manager/AnimalManager.ts
+++
@@ -1,8 +0,0 @@
-import { Scene, Vector3 } from "three";
-import { spawnObject } from "../functions";
-
-export class AnimalManager {
-  spawn(scene: Scene, position: Vector3, animalName: string) {
-    spawnObject(scene, position.x, 0, position.z, `${animalName}_1`);
-  }
-}
